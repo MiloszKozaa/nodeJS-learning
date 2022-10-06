f2e3207ff3019aac18a891985e7bbcf597f2925b
Amen't <milosz.koza12@gmail.com>
2022-10-06 17:22:36 +0200

    Chapter 7: Middleware

--- server.js
+++ server.js
@@ -1,8 +1,37 @@
-const { nextDay } = require('date-fns');
 const express = require('express');
 const app = express();
 const path = require('path');
+const cors = require('cors');
+const { logger } = require('./middleware/logEvents');
+const errorHandler = require('./middleware/errorHandler');
 const PORT = process.env.PORT || 3500;
+
+app.use(logger);
+
+const whiteList = [
+  'https://www.yoursite.com',
+  'http://127.0.0.1:5500',
+  'http://localhost:3500',
+];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whiteList.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('^/$|/index(.html)?', (req, res) => {
   res.sendFile(path.join(__dirname, 'views', 'index.html'));
@@ -27,11 +56,18 @@ app.get(
   }
 );
 
-
-
-app.get('/*', (req, res) => {
-  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+app.all('/*', (req, res) => {
+  res.status(404);
+  if (req.accepts('html')) {
+    res.sendFile(path.join(__dirname, 'views', '404.html'));
+  } else if (req.accepts('json')) {
+    res.json({ error: '404 not found' });
+  } else {
+    res.type('txt').send('404 not found');
+  }
 });
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server starts on port ${PORT}`);
